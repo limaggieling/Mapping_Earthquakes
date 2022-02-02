af34b8b822eb4bedf60ac92bf67ed76f5afd4013
lingli3 <li.maggie.ling@gmail.com>
2022-02-02 15:00:07 -0500

    removed api key from config files

--- Earthquake_Challenge/static/js/config.js
+++ Earthquake_Challenge/static/js/config.js
@@ -1,1 +1,1 @@
-const API_KEY = "pk.eyJ1IjoibGluZ2xpMyIsImEiOiJja3lhcGNubXAwN3M5MzBsZHd4MHoxYnd1In0.JLfhhqc11epB-OQAGdqj9Q"
+const API_KEY =
--- simple_map/static/js/config.js
+++ simple_map/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1IjoibGluZ2xpMyIsImEiOiJja3lhcGNubXAwN3M5MzBsZHd4MHoxYnd1In0.JLfhhqc11epB-OQAGdqj9Q";
+const API_KEY = "";
